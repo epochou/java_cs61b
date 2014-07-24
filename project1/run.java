73340918cb4d7c12ef93cd952ecc47a0961e8c56
xinlei zhou <xinlei_zhou@brown.edu>
2014-07-24 15:33:58 +0800

    project1 done

---
+++ project1/run.java
@@ -0,0 +1,15 @@
+public class run {
+   
+	public Color p;
+	public int count;
+	public  run next;
+	public  run prev;
+	
+	public run() {
+		p = new Color();
+		count = 0;
+		next = null;
+		prev = null;
+	}
+	
+	}
